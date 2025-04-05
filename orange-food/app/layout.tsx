214b69130115748945ac56f63b0c34191f93f790
Furkan Aslan <furkanaslan1209@gmail.com>
2025-04-05 18:07:18 +0300

    add @vercel/analytics dependency and integrate Analytics component in layout

--- orange-food/app/layout.tsx
+++ orange-food/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,6 +23,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Analytics></Analytics>
           {children}
         </ThemeProvider>
       </body>
